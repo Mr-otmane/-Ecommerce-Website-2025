0b6f5b546bdc265cec11b2abe61c0c04198df0d0
Mr otmane <otmaneanna2020@gmail.com>
2025-09-05 23:12:39 +0100

    put new commint inside this file

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const react = () => {
-  return (
-    <div>react</div>
-  )
-}
+const App = () => {
+  return <div>App</div>;
+};
 
-export default react
+export default App;
